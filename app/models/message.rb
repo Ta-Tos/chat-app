cecf205a3d82b56f7984fff59cb6b4980d9bd689
Ta-Tos <brotherhoodmusebiz@gmail.com>
2022-06-26 17:07:05 +0900

    Active Atorageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image #messagesテーブルとActiveStrageテーブルで管理された画像ファイルをアソシエーション
+
   validates :content, presence: true
 end
